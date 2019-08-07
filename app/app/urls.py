1f3fdd3ce1c0255ecc7fd189d7e3b395c6aaa9c1
Ahmed Shahwan <ahmed@shahwan.me>
2019-08-07 05:22:35 +0200

    added working list tags endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/user/", include("user.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls")),
+    path("api/recipe/", include("recipe.urls")),
+]
--- app/core/admin.py
+++ app/core/admin.py
@@ -20,3 +20,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
