06d6cadf96ba0d14b3ca001791169410b90cd6bd
Ahmed Shahwan <ahmed@shahwan.me>
2019-08-08 11:59:42 +0200

    list ingredients

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -42,3 +42,10 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=sample_user(), name="Vegan")
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(), name="Cucumber"
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
