3f7e3b9467bd1c4969c2000e31bd37ca65fa3800
Ahmed Shahwan <ahmed@shahwan.me>
2019-08-11 16:52:38 +0200

    recipe image upload

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -40,3 +40,12 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer to uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ("id", "image")
+        read_only_fields = ("id",)
