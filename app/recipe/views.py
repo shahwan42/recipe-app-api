553da637a8a04539dd1168e2574b28717a758666
Ahmed Shahwan <ahmed@shahwan.me>
2019-08-11 04:14:14 +0200

    List recipes view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 
@@ -35,3 +35,16 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage Recipes in the database"""
+
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
